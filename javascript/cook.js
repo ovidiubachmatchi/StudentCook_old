0d83d8e116afd18332731a9eb0a402c5b37b9712
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2021-05-06 16:07:09 +0300

    update

---
+++ javascript/cook.js
@@ -0,0 +1,36 @@
+var ingredients_list = new Set()
+
+function put_ingredient(ingredient){
+    if (!(ingredient == ""))
+    ingredients_list.add(ingredient)
+    show_ingredients()
+}
+function show_ingredients(){
+    var val = ""
+     if (ingredients_list.size == 0)
+     document.getElementById("ingredients_section").innerHTML = "There are no ingredients"
+     else
+     {
+        for (item of ingredients_list.values())
+        {
+        var button = `<span class="close" onclick="delete_ingredient('`+item+`')"><i class="fas fa-trash"></i></span>`
+        val += "<li>"+button+item+"</li>";
+        }
+        document.getElementById("ingredients_section").innerHTML = val
+     }
+}
+function delete_ingredient(ingredient){
+    var res = ingredient.slice(0, 5);
+    console.log(ingredient.slice(1,2))
+    ingredients_list.delete(ingredient)
+    show_ingredients()
+}
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("page").style.display = "none";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("page").style.removeProperty("display");
+  } 
